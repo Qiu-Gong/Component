6c529d2f5a1db41d2a9b21041869a8492ed04708
gong qiu <gong qiu>
2020-02-26 18:15:34 +0800

    [CHG] app 初始化各个组件的 Application

---
+++ app/src/main/java/com/qiugong/component/MainApplication.java
@@ -0,0 +1,46 @@
+package com.qiugong.component;
+
+import android.app.Application;
+
+import com.qiugong.base.AppConfig;
+import com.qiugong.base.BaseApp;
+
+/**
+ * @author qzx 20/2/26.
+ */
+public class MainApplication extends BaseApp {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        initModuleApp(this);
+        initModuleData(this);
+    }
+
+    @Override
+    public void initModuleApp(Application application) {
+        for (String moduleApp : AppConfig.moduleApps) {
+            try {
+                Class clazz = Class.forName(moduleApp);
+                BaseApp baseApp = (BaseApp) clazz.newInstance();
+                baseApp.initModuleApp(this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void initModuleData(Application application) {
+        for (String moduleApp : AppConfig.moduleApps) {
+            try {
+                Class clazz = Class.forName(moduleApp);
+                BaseApp baseApp = (BaseApp) clazz.newInstance();
+                baseApp.initModuleData(this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ base/src/main/java/com/qiugong/base/AppConfig.java
@@ -0,0 +1,9 @@
+package com.qiugong.base;
+
+public class AppConfig {
+    private static final String LoginApp = "com.qiugong.login.LoginApp";
+
+    public static String[] moduleApps = {
+            LoginApp
+    };
+}
