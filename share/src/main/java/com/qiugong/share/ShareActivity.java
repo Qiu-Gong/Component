01c457033c0863556986edf6e98cdebdd3f0e9a6
gong qiu <gong qiu>
2020-02-26 18:03:05 +0800

    [CHG] share 获取登陆接口

--- share/src/main/java/com/qiugong/share/ShareActivity.java
+++ share/src/main/java/com/qiugong/share/ShareActivity.java
@@ -1,8 +1,12 @@
 package com.qiugong.share;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.qiugong.connector.ServiceFactory;
 
 public class ShareActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class ShareActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                share();
+            }
+        });
+    }
+
+    private void share() {
+        if (ServiceFactory.getInstance().getAccountService().isLogin()) {
+            Toast.makeText(this, "分享成功", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "分享失败：用户未登录", Toast.LENGTH_SHORT).show();
+        }
     }
 }
